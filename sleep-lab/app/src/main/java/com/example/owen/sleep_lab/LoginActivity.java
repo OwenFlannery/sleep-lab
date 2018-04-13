ac13811ebd3f069d78dea7cc514b482a548f11b4
OwenFlannery <owen.flannery22@gmail.com>
2018-04-13 10:44:32 +0100

    update to bluetooth classes

--- sleep-lab/app/src/main/java/com/example/owen/sleep_lab/LoginActivity.java
+++ sleep-lab/app/src/main/java/com/example/owen/sleep_lab/LoginActivity.java
@@ -1,12 +1,9 @@
 package com.example.owen.sleep_lab;
 
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
-import android.annotation.TargetApi;
+
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Build;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.KeyEvent;
@@ -21,6 +18,7 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -42,6 +40,7 @@ public class LoginActivity extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
+        FirebaseApp.initializeApp(this);
         mAuth = FirebaseAuth.getInstance();
         if(mAuth.getCurrentUser() !=null)
         {
--- sleep-lab/app/src/main/java/com/example/owen/sleep_lab/RecordActivity.java
+++ sleep-lab/app/src/main/java/com/example/owen/sleep_lab/RecordActivity.java
@@ -2,44 +2,95 @@ package com.example.owen.sleep_lab;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
-import android.bluetooth.BluetoothProfile;
-import android.renderscript.Element;
+import android.bluetooth.BluetoothServerSocket;
+import android.bluetooth.BluetoothSocket;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
-
-import com.google.android.gms.common.api.PendingResult;
-import com.google.android.gms.common.api.Status;
-
-import java.util.List;
+import android.widget.Toast;
+import java.util.ArrayList;
+import java.util.Set;
 
 public class RecordActivity extends AppCompatActivity
 {
-
+    private BluetoothAdapter mBluetoothAdapter;
     private Button startRecordingBtn, stopRecordingBtn;
-    @Override
+    int REQUEST_ENABLE_BT =1;
+    String deviceName,deviceMacAddress;
+    public BluetoothServerSocket mServerSocket;
+    public ArrayList deviceItemList;
+
     protected void onCreate(Bundle savedInstanceState)
     {
+
+        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_record);
-
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        registerReceiver(mReceiver, filter);
         startRecordingBtn =(Button)findViewById(R.id.statBtn);
         //stopRecordingBtn =(Button)findViewById(R.id.stopBtn);
+
+        if (mBluetoothAdapter == null)
+        {
+            Toast.makeText(this, "Bluetooth is not available on this device", Toast.LENGTH_SHORT).show();
+        }
+
+        if (!mBluetoothAdapter.isEnabled())
+        {
+            Intent enableBtIntent = new Intent(mBluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
+       /* discoverable();
+        mBluetoothAdapter.cancelDiscovery();//use after discovery finished to reserve battery power
+    */
+        Log.d("devicelistr", "super called for deviceListFragment oCreate\n");
+
+        Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+
+        if(pairedDevices.size()>0)
+        {
+            for (BluetoothDevice device : pairedDevices)
+            {
+
+            }
+        }
     }
 
-    public void ScanForDevices()
+    public void discoverable()
     {
-        //scan for devices sensor.api
+        Intent discoverableIntent =
+                new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+        discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 120);
+        startActivity(discoverableIntent);
     }
 
-    public void startRecording()
+    private BroadcastReceiver mReceiver= new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                deviceName = device.getName();
+                deviceMacAddress = device.getAddress(); // MAC address
+            }
+        }
+    };
+
+    protected void onDestroy()
     {
-        //what actions to do while recording use recording.api
+        super.onDestroy();
+        // Don't forget to unregister the ACTION_FOUND receiver.
+        unregisterReceiver(mReceiver);
     }
 
     public void recordingStopped()
     {
         //recording stopped send file to firebase and save values to history.api
     }
-
 }
